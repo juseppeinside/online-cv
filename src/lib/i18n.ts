49584024cba0c201ffb1aebf19b88194d65c8f85
maxim <maksimshkiv@mail.ru>
2025-09-02 21:00:32 +0300

    fix: move i18n from public to src

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,8 +1,8 @@
 import i18next from 'i18next';
 import I18nextBrowserLanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import en from '../public/locales/en.json' with { type: 'json' };
-import ru from '../public/locales/ru.json' with { type: 'json' };
+import en from '../locales/en.json' with { type: 'json' };
+import ru from '../locales/ru.json' with { type: 'json' };
 
 i18next
   .use(I18nextBrowserLanguageDetector)
