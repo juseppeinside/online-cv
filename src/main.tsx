04814e264ea9b681a1a313cb40497b0d5c5a8d68
maxim <maksimshkiv@mail.ru>
2025-09-08 23:58:30 +0300

    feat: preloader

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,32 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import './lib/i18n.ts';
+import gsap from 'gsap';
 import App from './app.tsx';
 import Layout from './components/layout.tsx';
 
 // biome-ignore lint/style/noNonNullAssertion: <react rules>
-createRoot(document.getElementById('root')!).render(
+const root = createRoot(document.getElementById('root')!);
+
+root.render(
   <StrictMode>
     <Layout>
       <App />
     </Layout>
   </StrictMode>
 );
+
+const removePreloader = () => {
+  const preloader = document.getElementById('preloader');
+  if (preloader) {
+    gsap.to(preloader, {
+      opacity: 0,
+      scale: 1.1,
+      duration: 1,
+      ease: 'power3.inOut',
+      onComplete: () => preloader.remove(),
+    });
+  }
+};
+
+requestAnimationFrame(removePreloader);
