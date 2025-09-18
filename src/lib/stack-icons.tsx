173fe4a0ac842e7652c25c3266484c8a77e8de42
maxim <maksimshkiv@mail.ru>
2025-09-18 20:48:34 +0300

    feat: more my stack things

--- src/lib/stack-icons.tsx
+++ src/lib/stack-icons.tsx
@@ -1,24 +1,39 @@
+import AntDesignIcon from '../assets/icons/logos/antd.svg?react';
+import BiomeIcon from '../assets/icons/logos/biome.svg?react';
 import CSSIcon from '../assets/icons/logos/css.svg?react';
 import CursorIcon from '../assets/icons/logos/cursor-ico.svg?react';
 import DockerIcon from '../assets/icons/logos/docker.svg?react';
+import ESLintIcon from '../assets/icons/logos/eslint.svg?react';
+import ExpressIcon from '../assets/icons/logos/expressjs.svg?react';
+import FigmaIcon from '../assets/icons/logos/figma.svg?react';
 import GitIcon from '../assets/icons/logos/git.svg?react';
 import GSAPIcon from '../assets/icons/logos/gsap.svg?react';
 import HTMLIcon from '../assets/icons/logos/html.svg?react';
 import JestIcon from '../assets/icons/logos/jest.svg?react';
 import JavaScriptIcon from '../assets/icons/logos/js.svg?react';
+import LeafletIcon from '../assets/icons/logos/leaflet.svg?react';
+import LinearIcon from '../assets/icons/logos/linear.svg?react';
 import MapboxIcon from '../assets/icons/logos/mapbox.svg?react';
 import MobXIcon from '../assets/icons/logos/mobx.svg?react';
+import MuiIcon from '../assets/icons/logos/mui.svg?react';
 import NestJsIcon from '../assets/icons/logos/nestjs.svg?react';
 import NextJsIcon from '../assets/icons/logos/nextjs.svg?react';
 import NodeJsIcon from '../assets/icons/logos/nodejs.svg?react';
+import ObsidianIcon from '../assets/icons/logos/obsidian.svg?react';
+import PlaywrightIcon from '../assets/icons/logos/playwright.svg?react';
 import PostgreSQLIcon from '../assets/icons/logos/postgresql.svg?react';
+import PostmanIcon from '../assets/icons/logos/postman.svg?react';
+import PrettierIcon from '../assets/icons/logos/prettier.svg?react';
 import PrismaIcon from '../assets/icons/logos/prisma.svg?react';
 import RadixUI from '../assets/icons/logos/radixui.svg?react';
 import ReactIcon from '../assets/icons/logos/react.svg?react';
 import ReactHookFormIcon from '../assets/icons/logos/react-hook-form.svg?react';
 import ReduxIcon from '../assets/icons/logos/redux.svg?react';
+import StorybookIcon from '../assets/icons/logos/storybook.svg?react';
+import SwaggerIcon from '../assets/icons/logos/swagger.svg?react';
 import TailwindCSSIcon from '../assets/icons/logos/tailwindcss.svg?react';
 import TanStackIcon from '../assets/icons/logos/tanstack.svg?react';
+import TestingIcon from '../assets/icons/logos/testing-library.svg?react';
 import TypeScriptIcon from '../assets/icons/logos/ts.svg?react';
 import ViteIcon from '../assets/icons/logos/vite.svg?react';
 import WebpackIcon from '../assets/icons/logos/webpack.svg?react';
@@ -48,4 +63,19 @@ export const stackIcons = {
   NestJS: NestJsIcon,
   Jest: JestIcon,
   TanStack: TanStackIcon,
+  Obsidian: ObsidianIcon,
+  Figma: FigmaIcon,
+  Swagger: SwaggerIcon,
+  Postman: PostmanIcon,
+  'Testing Library': TestingIcon,
+  Playwright: PlaywrightIcon,
+  Biome: BiomeIcon,
+  ESLint: ESLintIcon,
+  Prettier: PrettierIcon,
+  AntDesign: AntDesignIcon,
+  leaflet: LeafletIcon,
+  Linear: LinearIcon,
+  'Material UI': MuiIcon,
+  express: ExpressIcon,
+  Storybook: StorybookIcon,
 };
